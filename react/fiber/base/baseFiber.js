9199a054640b25195d67cd5b65b268b6346b6d89
子木 <50632124+Ankera@users.noreply.github.com>
2021-06-21 00:14:54 +0800

    update performUnitOfWork

--- react/fiber/base/baseFiber.js
+++ react/fiber/base/baseFiber.js
@@ -1,3 +1,8 @@
+/**
+ * 从顶点开始遍历，
+ * 如果有儿子，先遍历第一个儿子，
+ */
+
 let A1 = { type: "div", key: "A1" };
 
 let B1 = { type: "div", key: "B1" };
@@ -17,54 +22,64 @@ C1.sibling = C2;
 C1.return = B1;
 C2.return = B1;
 
-function sleep(delay) {
-    for (let start = Date.now(); Date.now() - start <= delay;) { }
+function sleep (delay) {
+  for (let start = Date.now(); Date.now() - start <= delay;) { }
 }
 
 let rootFiber = A1;
-let nextUnitOfWork = null;
+// @ts-ignore
+let nextUnitOfWork = null; // 下个执行单元
 
-function workLoop(deadline) {
-    while ((deadline.timeRemaining() > 0 || deadline.didTimeout) && nextUnitOfWork) {
-        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
-    }
+let startTime = Date.now();
 
-    if (!nextUnitOfWork) {
-        console.log("render 阶段结束");
-        console.log("时间差值", Date.now() - startTime);
-    } else {
-        requestIdleCallback(workLoop, {
-            timeout: 1000
-        })
-    }
+function workLoop (deadline) {
+  while ((deadline.timeRemaining() > 0 || deadline.didTimeout) && nextUnitOfWork) {
+    // @ts-ignore
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+
+  if (!nextUnitOfWork) {
+    console.log("render 阶段结束");
+    // @ts-ignore
+    console.log("时间差值", Date.now() - startTime);
+  } else {
+    // @ts-ignore
+    requestIdleCallback(workLoop, {
+      timeout: 1000
+    })
+  }
 }
 
-function nextUnitOfWork(fiber) {
-    begin(fiber); // 处理 fiber
-    if (fiber.child) {
-        return fiber.child;
+// @ts-ignore
+function performUnitOfWork (fiber) {
+  beginWork(fiber); // 处理 fiber
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  // 如果没有儿子，则找兄弟结点
+  while (fiber) {
+    completeUnitOfWork(fiber);
+    if (fiber.sibling) {
+      return fiber.sibling; // 遍历弟弟
     }
-
-    while (fiber) {
-        completeUnitOfWork(fiber);
-        if (fiber.sibling) {
-            return fiber.sibling; // 遍历弟弟
-        }
-        fiber = fiber.return;
-    }
+    fiber = fiber.return;
+  }
 }
 
-function begin(fiber) {
-    sleep(20); // 占用20s
-    console.log("开始 ", fiber.key);
+function beginWork (fiber) {
+  sleep(20); // 占用20s
+  console.log("开始 ", fiber.key); // A1  B1 C1  C2
 }
 
-function completeUnitOfWork(fiber) {
-    console.log("结束 ", fiber.key);
+function completeUnitOfWork (fiber) {
+  console.log("结束 ", fiber.key); // A1  B1 C1  C2
 }
 
+// @ts-ignore
 nextUnitOfWork = rootFiber;
 
+// @ts-ignore
 requestIdleCallback(workLoop, {
-    timeout: 1000
+  timeout: 1000
 })
